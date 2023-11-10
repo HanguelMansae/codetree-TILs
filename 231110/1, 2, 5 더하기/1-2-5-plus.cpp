405378727dd6f5e0c74d86f18e4bc169a2c2f95f
HanguelMansae <101043296+HanguelMansae@users.noreply.github.com>
2023-11-10 10:24:29 +0900

    Title: 1, 2, 5 더하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 231110/1, 2, 5 더하기/1-2-5-plus.cpp
+++ 231110/1, 2, 5 더하기/1-2-5-plus.cpp
@@ -9,16 +9,14 @@ int main() {
     for(int i =0 ;i <= n; i++){
         arr[i] = 0;
     }
-    arr[0] = 0;
-    arr[1] = 1;
-    arr[2] = 1;
-    arr[5] = 1;
+    arr[0] = 1; // 0을 만들 수 있는 방법은 아무것도 더하지 않는다라는 1가지의 방법 존재 && 
     
-    for(int i =2; i<= n; i++){
-        for(int j = 0; j <= i; j++){
-            for(int k = 0; k < 3; k++){
-                if(j + list[k] == i){arr[i]+= arr[j]; arr[i] %= 10007;}
-            }
+    //arr[i] = 합이 i가 될 때 만들 수 있는 경우의 갯수
+    //=> arr[i]는 i-1,-2,-5를 통해 만들 수 있는 값들에 1,2,5를 더해서 만들어진 값이므로 arr[i-1, -2, -5]들의 합
+
+    for(int i =1; i<= n; i++){    
+        for(int j = 0; j < 3; j++){
+            if(i - list[j] >= 0){arr[i]+= arr[i - list[j]]; arr[i] %= 10007;}
         }
     }
     cout << arr[n];
