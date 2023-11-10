ec7451ba07407a1db2a9dbcd49fa135d257d3a48
HanguelMansae <101043296+HanguelMansae@users.noreply.github.com>
2023-11-10 17:03:07 +0900

    Title: 배낭 채우기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231110/배낭 채우기/knapsack.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+
+pair<int,int> item[101];
+int weightdp[101][10001];
+int main() {
+    int n, m;
+    cin >> n >> m;
+    for(int i =1;i <= n; i++){
+        int w, v;
+        cin >> w >> v;
+        item[i] = {w, v};
+    }
+    for(int i =0;i <= n; i++){
+        for(int j = 0; j <= m; j++){
+            weightdp[i][j] = INT_MIN;
+        }
+    }
+    weightdp[0][0] = 0;
+    
+    for(int i = 1; i <= n; i++){
+        for(int j = 0; j <= m; j++){
+            if(j - item[i].first >= 0 && weightdp[i-1][j-item[i].second] != INT_MIN){
+                weightdp[i][j] = max(weightdp[i][j], weightdp[i-1][j - item[i].second] + item[i].second);
+            }
+        }
+    }
+    int ans = INT_MIN;
+    for(int i = 1; i <= n; i++){
+        ans = max(ans, weightdp[i][m]);
+    }
+    cout << ans;
+    return 0;
+}
