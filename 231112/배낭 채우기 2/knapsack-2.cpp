bc8ccbd3f6ff8439660265f69e391f3ccdfd5747
HanguelMansae <101043296+HanguelMansae@users.noreply.github.com>
2023-11-12 19:55:01 +0900

    Title: 배낭 채우기 2, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 231112/배낭 채우기 2/knapsack-2.cpp
+++ 231112/배낭 채우기 2/knapsack-2.cpp
@@ -3,8 +3,8 @@
 using namespace std;
 
 int n, m;
-pair<int,int> list[101];
-int arr[10001];
+pair<int,int> list[101]; // 아이템의 무게와 가치를 담을 배열
+int arr[10001]; // 무게별로 최대 가치를 담을 배열 arr[j] => j만큼 무게를 담았을 때 최대 가치
 
 
 int main() {
@@ -24,6 +24,7 @@ int main() {
     for(int i = 0; i <= m; i++){
         for(int j =0 ; j < n; j++){
             if(i - list[j].first >= 0 && arr[i-list[j].first] != INT_MIN){
+                //만약 해당 아이템의 가치를 담기 전의 무게가 있다면 => 그 무게로 담을 수 있는게 있다면, 거기다가 아이템의 가치만큼 더한걸 현재 값이랑 비교해서 넣음
                 arr[i] = max(arr[i], arr[i-list[j].first] + list[j].second);
             }
         }
