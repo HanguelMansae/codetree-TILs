acabcaee5b68cc1f1d9034df9ee24c6ad4658e7b
HanguelMansae <101043296+HanguelMansae@users.noreply.github.com>
2023-11-23 00:56:12 +0900

    Title: 동일하게 분할, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 231123/동일하게 분할/equal-partition.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+bool dp[101][1001];
+int arr[101];
+int main() {
+    int n;
+    cin >> n;
+    int m = 0;
+    for(int i =0; i< n; i++){
+        cin >> arr[i];
+        m += arr[i];
+    }
+    int m = 0;
+    for(int i =0 ; i < n; i++){
+        dp[i][arr[i]] = true;
+    }
+    for(int i = 1;i < n; i++){
+        for(int j = 0; j < 1001; j++){
+            if(dp[i-1][j] == true){dp[i][j] = true;}
+            if(dp[i-1][j - arr[i]] == true){dp[i][j] = true;}
+        }
+    }
+    if(m %2 != 0){cout << "No";}
+    else if(dp[n-1][m/2]){cout << "Yes";}
+    else{cout << "No";}
+    return 0;
+}
